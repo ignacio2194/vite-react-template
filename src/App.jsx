d74ca778c776d3b06894c0a2fe955b072cc5ead1
Franco Martucci <franco.martucci.21@gmail.com>
2022-05-28 23:17:53 -0300

    Fix ESLint errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -11,7 +11,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button
+            type="button"
+            onClick={() => setCount((currentValue) => currentValue + 1)}
+          >
             count is: {count}
           </button>
         </p>
